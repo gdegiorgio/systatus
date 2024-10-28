5e055d31699f393c4c5ee446436861ec417d7b76
Arghyadeep Ghosh <60299552+Arghya721@users.noreply.github.com>
2024-10-29 01:52:35 +0530

    feat: add custom health check endpoint (#9)

--- examples/stdlib/main.go
+++ examples/stdlib/main.go
@@ -7,8 +7,14 @@ import (
 	"github.com/gdegiorgio/systatus"
 )
 
+func customHealthCheck(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Header().Add("Content-Type", "application/json")
+	fmt.Fprint(w, `{"status":"application is healthy"}`)
+}
+
 func main() {
-	opts := systatus.SystatusOptions{Prefix: "/dev", ExposeEnv: true}
+	opts := systatus.SystatusOptions{Prefix: "/dev", ExposeEnv: true, Healthcheck: customHealthCheck}
 	systatus.Enable(opts)
 	fmt.Println("Starting server on :3333")
 	if err := http.ListenAndServe(":3333", nil); err != nil {
