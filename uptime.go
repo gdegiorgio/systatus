dc870baa5f883bd7d0d0f4455b5e5e75ad3e2a77
Giovanni De Giorgio <giovannidegiorgio1999@gmail.com>
2024-11-24 18:44:24 +0100

    feat(uptime): add systeminfo call

--- uptime.go
+++ uptime.go
@@ -20,8 +20,10 @@ type UptimeResponse struct {
 func handleUptime(opts UptimeHandlerOpts) func(w http.ResponseWriter, r *http.Request) {
 	return func(w http.ResponseWriter, r *http.Request) {
 
-		var res UptimeResponse
+		res := UptimeResponse{}
 		var err error
+
+		log.Debug().Msgf("Handling uptime on %s machine", runtime.GOOS)
 
 		if runtime.GOOS == "windows" {
 			res, err = getWinUptime()
@@ -32,32 +34,34 @@ func handleUptime(opts UptimeHandlerOpts) func(w http.ResponseWriter, r *http.Re
 		if err != nil {
 			log.Err(err).Msg("Could not execute uptime on this host")
 			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 
-		w.WriteHeader(200)
 		w.Header().Add("Content-Type", "application/json")
 		err = json.NewEncoder(w).Encode(res)
 		if err != nil {
 			log.Err(err).Msg("Failed to encode response")
 			w.WriteHeader(http.StatusInternalServerError)
+			return
 		}
 	}
 }
 
 func getWinUptime() (UptimeResponse, error) {
-	log.Warn().Msg("If FastBoot is enabled, uptime may be tracked incorrectly")
+	log.Warn().Msg("If FastBoot is enabled, uptime on Windows may be tracked incorrectly")
+	cmdoutput, _ := exec.Command("systeminfo | find \"System Boot Time\"").Output()
+	log.Debug().Msg(string(cmdoutput))
 	return UptimeResponse{}, nil
 }
 
 func getUptime() (UptimeResponse, error) {
-	log.Debug().Msg("Handling uptime on unix machine")
 	cmdoutput, err := exec.Command("/usr/bin/uptime").Output()
 	if err != nil {
 		return UptimeResponse{}, err
 	}
 	splitCmd := strings.Split(string(cmdoutput), " ")
 	return UptimeResponse{
-		Systime: strings.TrimSpace(splitCmd[0]),
-		Uptime:  strings.TrimSpace(strings.Split(splitCmd[3], ",")[0]),
+		Systime: strings.TrimSpace(splitCmd[1]),
+		Uptime:  strings.TrimSpace(strings.Split(splitCmd[4], ",")[0]),
 	}, nil
 }
